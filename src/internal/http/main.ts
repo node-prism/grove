2a803ecdbb62bdcbbc2583446c988e22dffba145
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-18 18:00:36 -0600

    Add comments.

--- src/internal/http/main.ts
+++ src/internal/http/main.ts
@@ -16,6 +16,31 @@ import loadMiddleware from "../../internal/loader/middleware";
 import ExpressContext from "./context";
 import { GroveApp } from "../../shared/definitions";
 
+/**
+ * Given an Express route string and a request object, returns an object containing relevant information about the request.
+ * @param {string} route - The Express route string, potentially containing parameters and/or wildcards.
+ * @param {Request} req - The request object from an incoming HTTP request.
+ * @returns {Object} An object containing information about the request, including path parameters, query parameters, headers, and the bearer token (if any).
+ * @example
+ *
+ * const req = { 
+ *   params: { id: '123' }, 
+ *   query: { name: 'foo', age: '20' }, 
+ *   body: { email: 'foo@example.com' }, 
+ *   headers: { authorization: 'Bearer abc123', content-type: 'application/json' } 
+ * };
+ * const route = '/users/:id';
+ * const result = getRequestContext(route, req);
+ *
+ * // result object should be:
+ * // {
+ * //   path: { id: '123' },
+ * //   query: { name: 'foo', age: '20' },
+ * //   body: { email: 'foo@example.com' },
+ * //   headers: { authorization: 'Bearer abc123', content-type: 'application/json },
+ * //   bearer: 'abc123'
+ * // }
+ */
 function getRequestContext(route: string, req: Request) {
   const ret = {
     path: {},
@@ -76,6 +101,23 @@ function getWrappedHandler(handler: Function, route: string) {
   };
 }
 
+/**
+ * Creates route handlers for HTTP requests for a given module and route path.
+ * 
+ * @param module - The HTTP module that defines the handlers.
+ * @param middleware - The middleware to apply to all handlers.
+ * @param route - The route path.
+ * @param app - The Grove app.
+ * 
+ * @example
+ * const middleware = [throttleMiddleware, authMiddleware];
+ * const module = require("./myModule");
+ * createRouteHandlers(module, middleware, "/my-path", app);
+ * 
+ * This example will create route handlers for the "/my-path" route using the 
+ * handlers defined in the "myModule" module. The middleware defined in the
+ * `middleware` array will be applied to all of the handlers.
+ */
 function createRouteHandlers(
   module: HTTPModuleExports,
   middleware: any,
@@ -205,6 +247,29 @@ export default async function createHTTPHandlers(
   return defs;
 }
 
+/**
+ * Wraps an array of middleware functions with HTTP context, allowing them to interact with HTTP request and response objects.
+ *
+ * @param {string} route - The route to which the middleware will be added.
+ * @param {Function[]} middleware - The middleware to be wrapped.
+ * @returns {Function[]} - Returns a new array of middleware functions wrapped with HTTP context.
+ *
+ * @throws {Error} - Throws an error if any of the middleware fail to execute properly.
+ *
+ * @example
+ *
+ * const someMiddleware = (req, res, next) => {
+ *   console.log('This is some middleware.');
+ *   next();
+ * }
+ *
+ * const wrappedMiddleware = wrapMiddlewareWithHTTPContext('/my-route', [someMiddleware]);
+ * console.log(wrappedMiddleware); // [Function: handler]
+ * 
+ * The first argument provided to wrapped middleware is an ExpressContext object,
+ * and the second argument is the relevant request context, which looks like:
+ * { params: { ... }, query: { ... }, body: { ... }, headers: { ... }, bearer: "" }
+ */
 function wrapMiddlewareWithHTTPContext(route: string, middleware: Function[]) {
   return middleware.map((fn) => {
     function handler(req: Request, res: Response, next: NextFunction) {
@@ -225,6 +290,24 @@ function wrapMiddlewareWithHTTPContext(route: string, middleware: Function[]) {
   });
 }
 
+/**
+ * Converts a file path to a URL path, and ensures that it is a valid URL path.
+ *
+ * Examples:
+ *
+ * ```
+ * pathFromFilename('/app/routes/user/[userId].js')
+ * // -> '/app/routes/user/:userId'
+ *
+ * pathFromFilename('/app/routes/index.js')
+ * // -> '/app/routes'
+ * ```
+ *
+ * @param {string} filename The file path to convert to a URL path.
+ * @returns {string} The URL path corresponding to the provided file path.
+ * @throws {Error} Throws an error if the path parameters are invalid or duplicate.
+ * @throws {Error} Throws an error if a catch-all parameter (*) is not the last path parameter.
+ */
 function pathFromFilename(filename: string): string {
   const basename = path.basename(filename, path.extname(filename)).normalize();
   const directory = path.dirname(filename).normalize();
@@ -238,6 +321,18 @@ function pathFromFilename(filename: string): string {
   return renamed;
 }
 
+/**
+ * Replaces path parameter definitions in a filename with Express-style parameter
+ * names. Path parameter definitions are in square brackets and can optionally have
+ * a `...` prefix to indicate a variadic parameter. The function throws an error if
+ * the parameter name contains any characters other than letters, numbers, hyphens,
+ * or underscores, which are the only characters allowed in Express-style parameter
+ * names.
+ *
+ * @param filename - The filename to modify.
+ * @returns The modified filename with path parameters replaced with their corresponding
+ * Express-style parameter names.
+ */
 function renamePathProperties(filename: string): string {
   return filename.replace(/\[(\.{3})?(.*?)\]/gi, (_, variadic, name) => {
     if (!/^[a-z0-9_-]+$/i.test(name))
@@ -248,6 +343,22 @@ function renamePathProperties(filename: string): string {
   });
 }
 
+/**
+ * Ensures that path parameters in an Express route path are valid.
+ * Throws an error if two parameters have the same name, or if the catch-all
+ * parameter is not at the end of the path.
+ *
+ * @param path - The path to validate.
+ * @returns void.
+ * @throws an error if two parameters have the same name, or if the catch-all parameter is not at the end of the path.
+ *
+ * @example
+ * // This will throw an error because the path contains two parameters with the same name:
+ * validatePathParameters('/users/:id/:id');
+ *
+ * // This will throw an error because the catch-all parameter is not at the end of the path:
+ * validatePathParameters('/users/`*`/:id'); <-- (asterisk surrounded with `` because otherwise the comment will be closed)
+ */
 function validatePathParameters(path: string) {
   const keys: Key[] | undefined = [];
   pathToRegexp(path, keys);
