d8fbd99fab6491456cd91bc21c3558509adf4f19
Jonathan Pyers <pyersjonathan@gmail.com>
2023-04-04 17:52:12 -0600

    Refactor to simplify

--- src/internal/http/errors.ts
+++ src/internal/http/errors.ts
@@ -8,22 +8,11 @@ import { GroveApp } from "../../shared/definitions";
  * it is expected to export default an array of express-compatible
  * error handlers. These error handlers will be registered with core.app.
  */
-export default async function createErrorHandlers(app: GroveApp): Promise<any> {
+export default async function createErrorHandlers(app: GroveApp): Promise<void> {
   const errorsFile = path.join(app.root, "/errors.ts");
-  if (!existsSync(errorsFile)) {
-    return;
+  if (existsSync(errorsFile)) {
+    const { default: handlers } = await loadModule<{ default: (err: any, req: any, res: any, next: any) => void[] }>(errorsFile);
+    if (!Array.isArray(handlers)) throw new Error(`Expected errors.ts to export an array of error handlers.`);
+    handlers.forEach(handler => app.app.use(handler));
   }
-
-  const errors = await loadModule<{ default: (err: any, req: any, res: any, next: any) => void[] }>(errorsFile);
-  const handlers = errors.default;
-
-  if (!Array.isArray(handlers)) {
-    throw new Error(
-      `Expected errors.ts to export an array of error handlers.`
-    );
-  }
-
-  handlers.forEach(handler => {
-    app.app.use(handler);
-  });
 }
