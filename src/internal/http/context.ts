ba1ea06b8e98c2e1433dcbb58118871e4c1dc29a
Jonathan Pyers <pyersjonathan@gmail.com>
2023-04-04 17:55:37 -0600

    Simplify

--- src/internal/http/context.ts
+++ src/internal/http/context.ts
@@ -1,13 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
 export default class Context {
-  req: Request;
-  res: Response;
-  next: NextFunction;
-
-  constructor(req: Request, res: Response, next: NextFunction) {
-    this.req = req;
-    this.res = res;
-    this.next = next;
-  }
+  constructor(public req: Request, public res: Response, public next: NextFunction) {}
 }
